f61485a4bb42e58daeceb236af88a6ac7868a2a8
Matthew Migliore <mmigliore311@gmail.com>
2018-10-19 16:07:10 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## NYC Pigeon Organizer
+member## NYC Pigeon Organizer
 
 ## Objectives
 
--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  
+  data.each do |attribute, pigeon_data|
+    pigeon_data.each do |trait, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon] = {}
+        
+        data.keys.each do |pigeon_attribute|
+          pigeon_list[pigeon][pigeon_attribute] = []
+        end 
+      
+      end 
+    end 
+  end
+  
+  data.each do |attribute, pigeon_data|
+    pigeon_data.each do |trait, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon][attribute] << trait.to_s
+      end 
+    end 
+  end
+  
+  pigeon_list
 end
