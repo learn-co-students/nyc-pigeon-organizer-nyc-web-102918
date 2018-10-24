223115c5f37e9b4d17da4964df45ce6eadcce131
Ji S <j2yshin@gmail.com>
2018-10-24 21:34:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  pigeon_list = {}
+  
+  data.each do |attribute, attribute_data|
+    attribute_data.each do |k, v_array|
+      v_array.each do |name|
+        unless pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name].has_key?(attribute) == false
+          pigeon_list[name][attribute] = []
+          if v_array.include?(name)
+            pigeon_list[name][attribute] << k.to_s
+          end
+        else
+          pigeon_list[name][attribute] << k.to_s
+        end
+      end
+    end
+  end
+  pigeon_list
 end
