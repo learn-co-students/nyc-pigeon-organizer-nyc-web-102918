5ea179d9f4bd85548f2122dd5caef28e76c12a44
Henry <hanke.liu@gmail.com>
2018-10-24 12:18:44 -0400

    passed

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+
+  pigeon_list = {}
+
+  pigeon_data.each do |data_type, data|
+    data.each do |data_type_each, pigeon_names|
+
+      pigeon_names.each do |name|
+        if !pigeon_list[name].kind_of?(Hash)
+          pigeon_list[name] = {} # need a condition prevent overwrite.
+        end
+
+        if !pigeon_list[name][data_type].kind_of?(Array)
+          pigeon_list[name][data_type] = [] # need a condition from overwrite
+        end
+
+        pigeon_list[name][data_type].push(data_type_each.to_s) # assign each color to each name[color].
+      end
+
+    end
+  end
+
+  pigeon_list
 end
